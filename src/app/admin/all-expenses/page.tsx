05acae621ac54c29d3c4f0748cbfa98f66d57c43
Gaurav-pratap-CodeFusion <gauravpratap144@gmail.com>
2026-05-09 23:55:48 +0530

    Commit

--- src/app/admin/all-expenses/page.tsx
+++ src/app/admin/all-expenses/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import api from "@/services/api";
+import Link from "next/link";
 import toast from "react-hot-toast";
 
 export default function AllExpensesPage() {
@@ -30,13 +31,12 @@ export default function AllExpensesPage() {
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-2xl font-semibold">{expense.name}</h2>
               <span
-                className={`text-xs px-3 py-1 rounded-full font-medium ${
-                  expense.status === "APPROVED"
+                className={`text-xs px-3 py-1 rounded-full font-medium ${expense.status === "APPROVED"
                     ? "bg-green-100 text-green-700"
                     : expense.status === "REJECTED"
-                    ? "bg-red-100 text-red-700"
-                    : "bg-yellow-100 text-yellow-700"
-                }`}
+                      ? "bg-red-100 text-red-700"
+                      : "bg-yellow-100 text-yellow-700"
+                  }`}
               >
                 {expense.status}
               </span>
@@ -45,6 +45,14 @@ export default function AllExpensesPage() {
             <p className="mb-2"><strong>Amount:</strong> ₹{expense.amount}</p>
             <p className="mb-2"><strong>Date:</strong> {expense.expenseDate}</p>
             <p className="mb-4"><strong>Description:</strong> {expense.description}</p>
+            <div className="mt-5">
+              <Link
+                href={`/expenses/${expense.id}/comments`}
+                className="bg-black text-white px-4 py-2 rounded-lg inline-block"
+              >
+                Comments
+              </Link>
+            </div>
           </div>
         ))}
       </div>
